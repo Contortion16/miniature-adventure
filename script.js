084110f65d5f8cd1b3d741d3123e05f2bc6cba98
Contortion16 <alexandra.asherie@gmail.com>
2026-06-02 12:10:45 -0400

    Add YouTube video to Garment Construction tutorial

--- script.js
+++ script.js
@@ -267,6 +267,11 @@ const tutorials = [
             </ul>
             <h3>Fitting and Adjustments</h3>
             <p>Always do a muslin (test garment) for custom fits. Try on at each major step and mark adjustments before adding sleeves or hems.</p>
+            <h3>Video Tutorial</h3>
+            <p>Watch this helpful video for detailed garment construction techniques:</p>
+            <div class="video-container">
+                <iframe width="100%" height="400" src="https://www.youtube.com/embed/t3AtMXbzsOo?start=605" title="Garment Construction Tutorial" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
         `
     }
 ];
